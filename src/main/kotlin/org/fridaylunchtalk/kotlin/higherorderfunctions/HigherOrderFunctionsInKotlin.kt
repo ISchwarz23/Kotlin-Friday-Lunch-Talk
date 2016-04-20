e738239c7828b046c149c678768fa7fe3919ef89
Ingo Schwarz <schwarz-ingo@web.de>
2016-04-20 16:57:41 +0200

    Create higher order functions example

---
+++ src/main/kotlin/org/fridaylunchtalk/kotlin/higherorderfunctions/HigherOrderFunctionsInKotlin.kt
@@ -0,0 +1,23 @@
+package org.fridaylunchtalk.kotlin.higherorderfunctions
+
+/**
+ * Created by Ingo on 20.04.2016.
+ */
+fun <T, R> List<T>.mapIt(transform: (T) -> R): List<R> {
+    val result = arrayListOf<R>();
+
+    for (item in this) result.add(transform(item))
+
+    return result
+}
+
+fun transformToString(number: Int): String = "$number"
+
+
+fun main(args: Array<String>) {
+
+    val listOfNumbers = arrayListOf(1, 2, 3, 4, 5)
+    val listOfStrings = listOfNumbers.mapIt { ::transformToString }
+    val listOfDoubledNumbers = listOfNumbers.mapIt { it -> it * 2 }
+
+}
